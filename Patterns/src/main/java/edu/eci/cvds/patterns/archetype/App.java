a2fc93d79b6d34825c7b7156ca88e8efcee1a409
Juanvelandia-p <juan.velandia-p@mail.escuelaing.edu.co>
2025-01-30 15:45:04 -0500

    Primer comit del lab 2

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -0,0 +1,16 @@
+package edu.eci.cvds.patterns.archetype;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        if(args!=null){
+            System.out.println("JA JA");
+        }
+        else {
+            System.out.println("Hello world!");
+            System.out.println(args);
+        }
+    }
+}
